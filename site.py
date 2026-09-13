e4e630e2bc7cf7239677de22d5d9a75b2771b3f0
Vishal Ojha <vishal@vishalojha.me>
2026-09-13 14:53:14 +0530

    Polish Sheetal FM mobile station UI

--- site.py
+++ site.py
@@ -44,7 +44,7 @@ RATE_LIMIT = int(os.environ.get("RJSHEETAL_RATE_LIMIT", "3"))
 
 SHARED_TOKEN = os.environ.get("RJSHEETAL_TOKEN", "")
 ELEVENLABS_API_KEY = os.environ.get("ELEVENLABS_API_KEY", "")
-ELEVENLABS_VOICE_ID = os.environ.get("ELEVENLABS_VOICE_ID", "p9aflnsbBe1o0aDeQa97")
+ELEVENLABS_VOICE_ID = os.environ.get("ELEVENLABS_VOICE_ID", "hRKCLdG8a4wR6WPgMGQr")
 ELEVENLABS_MODEL_ID = os.environ.get("ELEVENLABS_MODEL_ID", "eleven_v3_conversational")
 ELEVENLABS_AGENT_ID = os.environ.get("ELEVENLABS_AGENT_ID", "")
 LISTENER_NAME = os.environ.get("RJSHEETAL_LISTENER_NAME", "Sheetal")

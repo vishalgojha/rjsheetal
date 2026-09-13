82b67ea1533f919213d78e9016e532f301b55377
Vishal <vishal@chaoscraftlabs.com>
2026-09-14 03:48:53 +0530

    Refresh assistant workspace with pastel task composer

--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'rjsheetal-shell-v6';
+const CACHE = 'rjsheetal-shell-v7';
 const SHELL = ['/', '/manifest.webmanifest', '/icon.svg', '/app.js', '/rj-lazy.js', '/spotify-personal.js'];
 
 self.addEventListener('install', event => {
